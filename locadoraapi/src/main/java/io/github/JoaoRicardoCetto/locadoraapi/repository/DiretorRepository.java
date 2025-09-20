00ba74595e76ced1f1d6a505a76f717a4ef1a994
JoaoRicardoCetto <joao.mcetto@gmail.com>
2025-09-20 11:57:46 -0300

    Adição das classes repository

--- locadoraapi/src/main/java/io/github/JoaoRicardoCetto/locadoraapi/repository/DiretorRepository.java
+++ locadoraapi/src/main/java/io/github/JoaoRicardoCetto/locadoraapi/repository/DiretorRepository.java
@@ -1,9 +1,9 @@
 package io.github.JoaoRicardoCetto.locadoraapi.repository;
 
-import io.github.JoaoRicardoCetto.locadoraapi.model.Titulo;
+import io.github.JoaoRicardoCetto.locadoraapi.model.Diretor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface TituloRepository extends JpaRepository<Titulo, UUID> {
+public interface DiretorRepository extends JpaRepository<Diretor, UUID>  {
 }
--- locadoraapi/src/main/java/io/github/JoaoRicardoCetto/locadoraapi/repository/ItemRepository.java
+++ locadoraapi/src/main/java/io/github/JoaoRicardoCetto/locadoraapi/repository/ItemRepository.java
@@ -1,9 +1,9 @@
 package io.github.JoaoRicardoCetto.locadoraapi.repository;
 
-import io.github.JoaoRicardoCetto.locadoraapi.model.Titulo;
+import io.github.JoaoRicardoCetto.locadoraapi.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface TituloRepository extends JpaRepository<Titulo, UUID> {
+public interface ItemRepository extends JpaRepository<Item, UUID>  {
 }
