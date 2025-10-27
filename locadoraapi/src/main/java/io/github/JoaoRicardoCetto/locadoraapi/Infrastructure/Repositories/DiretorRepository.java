8a78cfe92f8295b046b5b7349d2026ba2fed4335
JoaoRicardoCetto <joao.mcetto@gmail.com>
2025-10-27 15:08:22 -0300

    [FEAT] Adição de mappers e melhora da camada Presentation

--- locadoraapi/src/main/java/io/github/JoaoRicardoCetto/locadoraapi/Infrastructure/Repositories/DiretorRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.JoaoRicardoCetto.locadoraapi.Infrastructure.Repositories;
-
-import io.github.JoaoRicardoCetto.locadoraapi.Infrastructure.Common.IBaseRepository;
-import io.github.JoaoRicardoCetto.locadoraapi.model.Entities.Diretor;
-
-public interface DiretorRepository extends IBaseRepository<Diretor> {
-}
--- locadoraapi/src/main/java/io/github/JoaoRicardoCetto/locadoraapi/Infrastructure/Repositories/TituloRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.JoaoRicardoCetto.locadoraapi.Infrastructure.Repositories;
-
-import io.github.JoaoRicardoCetto.locadoraapi.Infrastructure.Common.IBaseRepository;
-import io.github.JoaoRicardoCetto.locadoraapi.model.Entities.Titulo;
-
-public interface TituloRepository extends IBaseRepository<Titulo> {
-}
